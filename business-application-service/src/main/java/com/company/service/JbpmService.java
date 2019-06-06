c566d9831b0977f9f3e12096a895e740857996e1
Vinicius Scheidegger <vinicius.scheidegger@gft.com>
2019-06-06 11:38:38 +0200

    Add processService completion method and modify properties for async

---
+++ business-application-service/src/main/java/com/company/service/JbpmService.java
@@ -0,0 +1,31 @@
+package com.company.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jbpm.services.api.DeploymentService;
+import org.jbpm.services.api.ProcessService;
+import org.jbpm.services.api.model.DeployedUnit;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+@Slf4j
+@Service
+public class JbpmService {
+
+    @Autowired
+    private ProcessService processService;
+
+    @Autowired
+    private DeploymentService deploymentService;
+
+    public void completeWorkItem(Long processInstanceId, Long workItemId, Map<String, Object> result) {
+
+        ArrayList<DeployedUnit> deployedUnits = new ArrayList<>(deploymentService.getDeployedUnits());
+        String containerId = deployedUnits.get(0).getDeploymentUnit().getIdentifier();
+        log.info("deployed unit: " + containerId);
+        this.processService.completeWorkItem(containerId, processInstanceId, workItemId, result);
+
+    }
+}
