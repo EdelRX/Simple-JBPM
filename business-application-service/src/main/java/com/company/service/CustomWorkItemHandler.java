951c785c5dc6f389569e792e4f6a08709fd5c0db
Vinicius Scheidegger <vinicius.scheidegger@gft.com>
2019-06-06 11:37:38 +0200

    Complete workitem

--- business-application-service/src/main/java/com/company/service/CustomWorkItemHandler.java
+++ business-application-service/src/main/java/com/company/service/CustomWorkItemHandler.java
@@ -1,17 +1,24 @@
 package com.company.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.kie.api.runtime.process.WorkItem;
 import org.kie.api.runtime.process.WorkItemHandler;
 import org.kie.api.runtime.process.WorkItemManager;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
 @Component("CustomWorkItemHandler")
 public class CustomWorkItemHandler implements WorkItemHandler{
 
 	@Override
 	public void executeWorkItem(WorkItem workItem, WorkItemManager manager) {
 		// TODO Auto-generated method stub
-		
+		Map<String, Object> map = new HashMap<>();
+		manager.completeWorkItem(workItem.getId(), map);
+		log.info("Custom Work Item Completed");
 	}
 
 	@Override
